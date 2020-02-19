81957f70660dc618e40bcd64f6479fe1a24893a6
aladis <vlmandzhiev@gmail.com>
2020-02-19 21:40:24 +0300

    feat: decription task Reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    m = Math.abs(n) + "";
+    return +m
+        .split("")
+        .reverse()
+        .join("");
+};
